c23c778929924d700c537f70f2f6d662188deb36
colmf1 <colmfearon@hotmail.com>
2026-02-02 23:08:54 +0000

    lol cant print an iterator watch this space

---
+++ cpluspoker/main.cpp
@@ -0,0 +1,32 @@
+
+// Step1:
+// Implement a deck class, with shuffle and deal methods.
+// Deck is made up of cards 
+// I think I make a card class first, then maybe hide it in deck later.
+// A card is a struct 
+// No it isn't because I want to print it 
+// Nevermind, I can just make a function that takes a card
+
+
+
+// No idea about anything, work my way through, dont touch cpp folder or clankers.
+// This version will track gamestate.
+
+#include <iostream>
+
+int main() {
+    struct card {
+        // Abandoning chars because im bad at coding
+        int value;
+        int suit;
+    };
+
+    // Face 1-4
+    // Suit 
+
+    for(int i; i=0; i<52) {
+        std::cout << i << '\n';
+    }
+
+    return 0;
+}
