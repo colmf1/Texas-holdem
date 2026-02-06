8e9023a1f878282ed55911fdc8d8c0d4400cc4b3
colmf1 <colmfearon@hotmail.com>
2026-02-06 19:38:02 +0000

    figured out my card and deck anyway - left notes v cool

--- cpluspoker/main.cpp
+++ cpluspoker/main.cpp
@@ -1,32 +1,25 @@
-
-// Step1:
-// Implement a deck class, with shuffle and deal methods.
-// Deck is made up of cards 
-// I think I make a card class first, then maybe hide it in deck later.
-// A card is a struct 
-// No it isn't because I want to print it 
-// Nevermind, I can just make a function that takes a card
-
-
-
-// No idea about anything, work my way through, dont touch cpp folder or clankers.
-// This version will track gamestate.
-
 #include <iostream>
+#include <string>
+#include <array>
 
 int main() {
-    struct card {
-        // Abandoning chars because im bad at coding
-        int value;
-        int suit;
-    };
-
-    // Face 1-4
-    // Suit 
-
-    for(int i; i=0; i<52) {
-        std::cout << i << '\n';
+    // I think this is pretty cool
+    std::array<int, 52> deck;
+    for (int i=0; i<52; i++){
+        std::cout << std::to_string(i) << ": ";
+        int rank = i/4;
+        int suit = i%4;
+        
+        // When defining our card, We take the bits of the rank, i.e. 3 = 11, and move them over by 2
+        // Then we concatenate suit, 3 of spades = 1100
+        deck[i] = (rank << 2) | suit;
+        std::cout << std::to_string(rank) << std::to_string(suit) << " = " << 
+            // You get the rank and suit back by these 
+            // For rank, we just chuck off the suit bits with >> 2 just the reverse
+            // For suit, we just mask all but the last two bits with this magic 
+            // & 0b11 = mask all but the last two bits
+            // & 3 also works, because 0b means binary 3
+            std::to_string(deck[i] >> 2) << std::to_string(deck[i] & 0b11) << "\n";
     }
-
-    return 0;
+   return 0;
 }
