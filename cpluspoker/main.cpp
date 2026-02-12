e40891fa4c789adb70cd0f18b3ef5b59d4bbe550
colmf1 <colmfearon@hotmail.com>
2026-02-12 20:55:24 +0000

    gonna tidy into funcs

--- cpluspoker/main.cpp
+++ cpluspoker/main.cpp
@@ -3,51 +3,22 @@
 #include <random>
 #include <algorithm>
 
-// Don't pass in pointers - pass in the actual gamestate 
-// Pointers are low memory, but cause bouncing about in memory
-// If we minimise the size of our gamestate we can just copy
-// Asked claude about cache locallity - keeping the deck will
-// just pollute the cache anyway - keep it separate, we only use it to deal cards
-//
-// struct Gamestate{    
-//     // Deck is just 52 number array
-//
-//     // hands stored together as a matrix
-//     // p0=hand[0][0:1]
-//     int hand[2][2];
-//     int table[5];
-//
-//     int pot, round;
-//     int chips[2];
-//     int bets[2];
-//
-//     bool p0;
-//     uint32_t history; // This needs to be 32 bit encoded for bit stuff
-//     // history gonna be the focus of this
-//     // we need to track the actions for as far back as possible
-//     // minimise memory usage
-//     //
-// };
-//
-
 struct Gamestate{
     int hand[2][2];
     int table[5];
     int pot, round;
     int chips[2];
     int bets[2];
-    bool p0; // p0 true/false - to track current player
+    bool id; 
+    bool bb;
     uint32_t history; 
 };
-
-void change_player(Gamestate& game){
-    game.p0 = !game.p0;
-}
 
 void init_gs(Gamestate& game){
     game.pot = 0;
     game.round = 0;
-    game.p0 = false;
+    game.id = 0;
+    game.bb = 0;
 }
 
 std::array<int, 52> create_deck(){
@@ -78,14 +49,78 @@ void deal_cards(Gamestate& game, std::array<int,52>& deck){
     game.table[4] = deck[8];
 }
 
-void card_str(int card){
+std::string card_str(int card){
+    // I should just return a string?
     std::array<std::string, 4>  suits ={
         "Spades", "Clubs", "Hearts", "Diamonds"
     };
     std::array<std::string, 13> ranks = {
         "2","3","4","5","6","7","8","9", "10", "Jack", "Queen", "King", "Ace"
     };
-    std::cout << ranks[card/4] << " of " << suits[card/12] << "\n";
+    return to_string(ranks[card/4] | " of " | to_string(suits[card/12])
+}
+
+void show_bets(Gamestate& game)
+    std::cout << "Bets - 0:" << game.bets[game.id] << ", 1:" << game.bets[game.id] << "\n";
+}
+
+void show_hand(Gamestate& game, int player){
+   if (round != 0){
+        std::cout << "Table\n";
+        for (int i = 0; i < round + 2; i++){
+            std::cout << card_str(game.table[i]) << "\n";
+            }
+        } 
+    
+    std::cout << "Hand\n" << card_str(game.hand[player][0]) << "\n" << card_str(game.hand[player][1]) << "\n\n"
+}
+
+
+void show_info(Gamestate& game, int player){
+    show_bets(game);
+    show_hands(game);
+        // I should make show bets, show hand and show table which uses round 
+        std::cout << "Hand\n";
+        card_str(game.hand[player][0]); 
+        card_str(game.hand[player][1]);
+        std::cout << "\n";
+
+        // table cards too 
+ 
+    }
+}
+
+void handle_action(Gamsetate& game){
+    // placeholder for bot action
+    
+    // instead of checking for call, check if bets are equal.
+    // call and check will be identical to model
+    // Show info function
+    int action;
+    if (game.bets[0]==game.bets[1]){
+        std::cout << "0:fold \n1:call \n2:raisehalf \n3:raisepot \n4:Allin\n"; 
+    }else{
+        std::cout << "0:fold \n1:check \n2:raisehalf \n3:raisepot \n4:Allin\n"; 
+    }
+    std::cin >> action;
+    // if action is 0, run function end round
+    // add pot to the winning player !id or whatever
+    // ends the round while loop
+    // 
+    // 
+    // there should be encoding here to separate call and check
+}
+
+void handle_action(int action){
+
+}
+
+void preflop(Gamestate& game){
+    while (true) {
+        // Ill fix it later
+        handle_action(game);
+
+    }
 }
 
 int main() {
@@ -96,31 +131,31 @@ int main() {
     game.chips[1] = 5000;
 
     while (true){
+        bool bb = 0;
+        
         // only break after run out of chips.
         // give a quit option with the other actions for now 
         init_gs(game);
         deal_cards(game, deck);
         //blinds 
-        game.bets[game.p0]  = 1;
-        game.bets[!game.p0] = 2;
-
-        // preflop
-
-        std::cout << "Bets - 0:" << game.bets[game.p0] << ",1:" << game.bets[game.p0] << "\n";
-        std::cout << "Hand\n";
-        card_str(game.hand[0][0]); 
-        card_str(game.hand[0][1]);
-        std::cout << "\n";
-
+        game.bets[!game.bb] = 2;
+        game.bets[game.bb]  = 1;
 
         
-        // Ask for input 
-        // Check if it's a fold or call round 
-        // Make a map of inputs to actions - do it later
-        int action;
-        std::cout << "0:fold \n1:call \n2:raisehalf \n3:raisepot \n4:Allin\n"; 
-        std::cin >> action;
-        std::cout << "Action was " << action << "\n";
+
+        // preflop
+        // function that takes gamestate as input and shits these out 
+               if (action == 0){
+            // ends the round - we need another while 
+        }
+        if (action == 1){
+            // if it's a call, end the round since we have only two players
+        }
+        // doesn't exist yet 
+        check_endgame();
+        bb = !bb;
         return 0;
-        } 
+    }
 }
+
+
