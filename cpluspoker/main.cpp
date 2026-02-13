0164a366a775a3968100fede3d79676b51c329d6
colmf1 <colmfearon@hotmail.com>
2026-02-13 20:10:13 +0000

    moving machines

--- cpluspoker/main.cpp
+++ cpluspoker/main.cpp
@@ -4,10 +4,6 @@
 #include <random>
 #include <string>
 
-// This is all the info we need
-// I track big blind outside of the gamestate
-// everything else is here
-
 // This could all live elsewhere really
 struct Gamestate {
   int hand[2][2];
@@ -15,12 +11,13 @@ struct Gamestate {
   int round;
   int chips[2];
   int bet[2];
-  bool id;
+  // id didnt even get used lol theres no point tracking it
+  // just store it as an index, and move player around
+  // player references the index of the lists, as does bb
   bool player;
   bool bb;
   uint32_t history; // deal with this later
 };
-
 // Human io
 std::string card_str(int card) {
   // I should just return a string?
@@ -32,8 +29,8 @@ std::string card_str(int card) {
 }
 
 void show_bets(Gamestate &game) {
-  std::cout << "Bets - 0:" << game.bet[game.id] << ", 1:" << game.bet[game.id]
-            << "\n";
+  std::cout << "Bets - 0:" << game.bet[game.player]
+            << ", 1:" << game.bet[!game.player] << "\n";
 }
 
 void show_hand(Gamestate &game) {
@@ -104,7 +101,6 @@ void deal_cards(Gamestate &game, std::array<int, 52> &deck) {
 // Gamestate
 void init_gs(Gamestate &game) {
   game.round = 0;
-  game.id = 0;
   game.player = 0;
   game.chips[0] = 5000;
   game.chips[1] = 5000;
@@ -116,104 +112,138 @@ void bresets(Gamestate &game) {
   game.bet[1] = 0;
 }
 
-void fold(Gamestate &game) {
+bool fold(Gamestate &game) {
   game.chips[!game.player] += game.bet[0] + game.bet[1];
   bresets(game);
+  return false;
 }
 
-void call(Gamestate &game) {
+bool call(Gamestate &game) {
   game.chips[game.player] -= (game.bet[!game.player] - game.bet[game.player]);
-  ;
   game.bet[game.player] += (game.bet[!game.player] - game.bet[game.player]);
-  ;
-  // Pass over action - this needs to go back to the start of the function
   game.player = !game.player;
+  return true;
 }
 
-void bethpot(Gamestate &game) {
+bool bethpot(Gamestate &game) {
   game.chips[game.player] -=
       (game.bet[game.player] + game.bet[!game.player]) / 2;
   game.bet[game.player] += (game.bet[game.player] + game.bet[!game.player]) / 2;
   game.player = !game.player;
+  return true;
 }
 
-void betfpot(Gamestate &game) {
-  game.chips[game.player] -= game.bet[game.player] + game.bet[!game.player];
-  game.bet[game.player] += game.bet[game.player] + game.bet[!game.player];
-  game.player = !game.player;
-}
-
-void allin(Gamestate &game) {
-  show_options(game, 1);
-  int action = get_input();
-  if (action == 0) {
-    fold(game);
-  } else {
-    call(game);
-  }
-}
-
-int showdown(Gamestate &game) {
-  // not really sure if i cba with this one
-  // cba implementing hand eval from nums but ahwell
-  // anyway
-  // if we've got here we need to pick a winner
-  // make a hand eval func
-  // return index of loser
-  // force them to fold
-  return 0;
-}
-
-int hand_eval(Gamestate &game) {
-  int winner;
-  // ending for the night here
-  game.hand[0];
-  game.hand[1];
-
-  return winner;
-}
-
-void call_or_check(Gamestate &game) {
+bool call_or_check(Gamestate &game) {
   if (game.bet[0] == game.bet[1]) {
     game.player = !game.bb;
     game.round += 1;
+    return true;
+  }
+  return call(game);
+}
+
+bool betfpot(Gamestate &game) {
+  game.chips[game.player] -= game.bet[game.player] + game.bet[!game.player];
+  game.bet[game.player] += game.bet[game.player] + game.bet[!game.player];
+  game.player = !game.player;
+  return true;
+}
+
+bool allin(Gamestate &game) {
+  show_options(game, 1);
+  int action = get_input();
+  if (action == 0) {
+    return fold(game);
   } else {
-    call(game);
+    return call(game);
   }
 }
 
+// Need hand detector funcs
+// Need ranking of hands
+// Need draw handling
+// loser forced to fold
+
+//
+// def eval_flush(self,all_cards):
+//     suits = [card.show()[-1] for card in all_cards]
+//     values = [card.show()[:-1] for card in all_cards]
+//     value_map = {'A': 14, 'K': 13, 'Q': 12, 'J': 11}
+//     numeric_values = list(map(int, [value_map.get(v, v) for v in values]))
+//     lookup = {numeric_values[i]: suits[i] for i in range(len(suits))}
+//     suit_counts = Counter(suits)
+//     if max(suit_counts.values()) >= 5:
+//         flush_suit = [key for key, value in suit_counts.items() if value >=
+//         5][0] cards = [card for card, suit in lookup.items() if suit ==
+//         flush_suit] high_card = max(cards) card_str = next(k for k, v in
+//         value_map.items() if v == high_card) if high_card in
+//         value_map.values() else str(high_card) return cards, card_str
+//     else:
+//         return False
+//
+//
+//
+
+std::array<int, 2> hand_eval(Gamestate &game) {
+  std::array<int, 2> scores;
+  // Return two scores
+  // If they're equal do further comparison.
+  // First it goes highest rank of card within the winning combo
+  // Then highest other card
+  // I think this is the same for all hands so handle separately
+  return scores;
+}
+
+int high_card(Gamestate &game) { return 0; }
+
 bool play_round(Gamestate &game) {
+  // Full game control in here
+
+  // This function is ran inside a loop
+  // We exit the function with true or false
+  // False ends the loop, True restarts the loop
+
   if (game.round == 5) {
-    showdown(game);
+    // Check for r5 at start, showdown - all in skips to here
+    std::array<int, 2> scores = hand_eval(game);
+
+    if (scores[0] == scores[1]) {
+      return high_card(game);
+    }
+    return scores[0] < scores[1];
+    // winner = score0(20)<score1(15)=0 score0(10)<score1(15)=1
   }
 
+  // Show info round dependant
   show_info(game);
   int action = get_input();
 
   switch (action) {
   case 0:
-    fold(game);
-    return false;
+    // Fold returns false, pays the winner exits loop to endgame
+    return fold(game);
 
   case 1:
-    call_or_check(game);
-    return true;
+    // Call and check
+    // These are equivalent to bot
+    // Call changes player, check goes onto next round
+    // Only check advances the round
+    return call_or_check(game);
 
+  // Raises place bets and change player
   case 2:
-    bethpot(game);
-    return true;
-
+    return bethpot(game);
   case 3:
-    betfpot(game);
-    return true;
+    return betfpot(game);
 
   case 4:
-    allin(game);
-    return false;
-
+    // All in gives the other player a chance to call
+    // If they call returns true and sets round = 5 for a showdown
+    // if they fold it returns false
+    return allin(game);
   default:
     std::cout << "Invalid action\n";
-    return false;
+    return true;
   }
 }
 
@@ -235,8 +265,9 @@ int main() {
     while (!playing) {
       playing = play_round(game);
     }
+
     // someone wins the hand
-    // if we only wipe the losers hand in fold we know the winner
+    // winner is paid out in fold?
   }
   return 0;
 }
