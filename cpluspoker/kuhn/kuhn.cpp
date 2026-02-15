07f60a31c11ca06a537ad45272335d564d436d38
colmf1 <colmfearon@hotmail.com>
2026-02-15 22:21:51 +0000

    think thats kuhn poker nailed - kinda clueless how to implement cfr from here

--- cpluspoker/kuhn/kuhn.cpp
+++ cpluspoker/kuhn/kuhn.cpp
@@ -1,25 +1,10 @@
-
 #include <algorithm>
 #include <array>
 #include <iostream>
 #include <random>
-#include <string>
 
-// Kuhn poker has 3 cards
-// Jack < Queen < King
-// Options are pass/bet
-
-// I also needa sort tracking history in other class
-
-// This is the most annoying thing ever
-// I've just discovered it...
-// I know im gonna be writing a ton of int arrays
-// I can just fastcode them with arr
-
-// This is first line in my helper funcs script
+// Helper funcs
 template <std::size_t N> using arr = std::array<int, N>;
-// print - not wise
-// got from goog - needa figure out the ...s - seems self explanatory
 template <typename... Args> void print(Args &&...args) {
   ((std::cout << args << " "), ...);
   std::cout << '\n';
@@ -42,81 +27,61 @@ void shuffle_deck(arr<3> &deck) {
 struct Gamestate {
   arr<2> card;
   int player;
-  arr<2> action;
-  int round;
-  arr<4> history;
+  int action;
 };
 
-// I need to enumerate every single info state
-
-// All possible infostates for both players
-// p0
-// Initial - 0 + (0, 1, 2)
-// After pass 3 + (0,1, 2)
-// After bet 6 + (0,1,2)
-
-// p1
-// Initial - 9 + (0, 1, 2)
-// After pass 12 + (0,1, 2)
-// After bet 15 + (0,1,2)
-
-// From this 2 possible actions always.
-//
-
-// history +=
-
-// load of other combos? Do I needa mad for loop r smt
-// need to make unique ints
-
 int get_action() {
-  // I need to know prev actions to do this
+  // returns Pass = 3, Bet = 6
+  // doesn't really matter
   int action;
   std::cin >> action;
-  return action;
+  return (action + 1) * 3;
 }
 
-void showdown(Gamestate &game) {
-  // very simple just compare cards?
+int showdown(Gamestate &game) {
+  // returns index of winner
+  return game.card[0] < game.card[1];
 }
 
 int play_round(Gamestate &game) {
-  // This round ends and changes the active player
-  // Update the player every time we move
-  if (game.round == 3) {
-    showdown(game);
+
+  // Infostate is what the Bot will see
+  // Number between 0-18 - 3cards*3actions*2players (1st or second)
+
+  // I'm imagining that these will be nodes on our cfr
+  // Each infostate will have an associated float array strategy
+  // eg. [0.2, 0.8] = [P(pass), P(bet)]
+  // We initialise everything at [.5, .5]
+  // We make our decision
+  // We then calculate regret??
+  // Idk how that works
+  // Will need to calculate it every round
+
+  int infostate = (9 * game.player) + game.action + game.card[game.player];
+
+  print("Infostate", infostate);
+  print("Pass(0) or Bet(1)");
+  // Get action will be replaced with bot action
+  int action = get_action();
+
+  // unless its r0 - passing will end the round with no winner
+  if (!(game.action == 0) and (action == 3)) {
+    print("Passed - ended round");
     return 0;
   }
 
-  print("Player: ", game.player);
-  print(game.card[game.player]);
-  print("Bet or Pass");
-  int action = get_action();
-
-  // should we be using game.round?
-  // we lit needa track history
-  // history = [0,0,0]
-  // Needa distinguish p1 pass vs p2 pass - different information
-  //
-
-  if (action == pass) {
-    // send back to start of loop
-    return 1;
+  // double bets have a showdown
+  if (game.action == 6 and action == 6) {
+    int winner = showdown(game);
+    print("Winner", winner);
+    // I'll needa to some regret maxing here I assume
+    return 0;
   }
-  // Show card
-  // Bet or pass
-  // p1 sees card, p0s action
-  // bet or pass
-  // bet+bet showdown
-  // pass+pass draw
-  // bet+pass p0 win
-  // pass+bet+pass p1 win
-  // pass+bet+bet showdown
 
-  // actor is game.player - this gets changed after an action
-  // This depends on player
-  show_info(game)
-
-      return 0;
+  // Any other scenario it gets updated and we repeat
+  game.action = action;
+  game.player = !game.player;
+  return 1;
 }
 
 int main() {
@@ -124,20 +89,24 @@ int main() {
   arr<3> deck = create_deck();
   Gamestate game;
 
-  int playing = 1;
+  print("New Game");
+  int playing;
   int bb = 0;
   while (true) {
+    // round initialisation
+    print("New Round");
+    playing = 1;
     game.player = bb;
-    // deal
     shuffle_deck(deck);
     game.card = {deck[game.player], deck[!game.player]};
-    // I needa change order here
-    // before going into round loop we switch bb
+    game.action = 0;
 
+    // Play a round
     while (playing) {
       playing = play_round(game);
     }
+
+    bb = !bb;
   }
-  bb += 1;
   return 0;
 }
