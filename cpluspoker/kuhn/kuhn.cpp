3e627b6d88cbd7b22974c31eb17926f1c4ae9deb
colmf1 <colmfearon@hotmail.com>
2026-02-17 23:23:14 +0000

    what a mess

--- cpluspoker/kuhn/kuhn.cpp
+++ cpluspoker/kuhn/kuhn.cpp
@@ -1,174 +1,102 @@
-#include <algorithm>
 #include <array>
-#include <iostream>
-#include <random>
 
-// Whoops idk 3 card poker lol
-// My betting round is wrong
-// action are
+// CFR for kuhn poker.
+// We need a smart way of making unique ints from all combos
+// These are all possible decision states
+// I Loop through 0,1,2
+// int Strategy[12, 12] - can probably be compressed because it must add to 1
+// int cumRegret[12,12] - Regret = EV of always doing [pass, bet] - EV of
+// current strategy int EV[12,12] Expected payout. Assuming all strategies 50,50
+// Eg when in BK-Betting EV=2, Passing EV=-1
 
-// Pass-Pass: Showdown, high card wins
-// Pass-Bet-Call: Showdown, high card wins
-// Pass-Bet-Fold: Bettor wins pot
-// Bet-Call: Showdown, high card wins
-// Bet-Fold: Bettor wins pot
+// regret = [
+//            bet_payout - strategy*bet_payout,
+//            pass_payout -(1-strategy)+pass_payout
+// ]
+// The ratios of the above will be our new strategy - they increase over time
+// i.e. when pass doesnt pay off first go, regret= [2,0], new_strategy =
+// regret[0] /(2+0) = 2/2=1 strategy = payout_bet/all_payouts If a magic world
+// existed where a pass paid off second hand regret = [4,1] new_strat = Now it
+// bets always, since only positive regret goes up, the ratios always 1
 
-// Helper funcs
-template <std::size_t N> using arr = std::array<int, N>;
-template <typename... Args> void print(Args &&...args) {
-  ((std::cout << args << " "), ...);
-  std::cout << '\n';
-}
+// 2 - (.5*2 = 1)
 
-// Cards
-arr<3> create_deck() {
-  arr<3> deck;
-  for (int i = 0; i < 3; i++) {
-    deck[i] = i;
-  }
-  return deck;
-}
+// store 2 arrays
+// Cum regret and strategy
 
-void shuffle_deck(arr<3> &deck) {
-  std::mt19937 rng(std::random_device{}());
-  std::shuffle(deck.begin(), deck.end(), rng);
-}
+// Current strategy EV= (.5 * 2) + (.5 * -1) = 1+-.5 = .5
+// Betting 100% EV=
 
-struct Gamestate {
-  arr<2> card;
-  int player = 0;
-  int action = 0;
+// BQ-Betting EV = chance_of_higher/lower_card(.5)*4= +- 2 = 0
+
+// - Current strategy at BQ is to bet 50% of time
+// - Current EV = (.5*2)
+
+// When we first enter the loop, in order to make a decision
+// we have to calculate the expected payout of making that decision
+// both options however are unknown, but calculateable
+// We first
+
+// This is just for us - its not used.
+// >5 is terminal - can calculate all returns there
+// In both decisions we can calculate regret.
+
+// We need a funciton that calculates EV
+// If the node > 5 we return ints
+// Otherwise we recursivly return our function
+
+// At each stage, we calculate regret
+// Regret is EV of doing an action 100% of time - EV of current strategy
+// At
+
+std::array<int, 12> node = {
+    // p0
+    0, // J
+    1, // Q
+    2, // K
+    // p1
+    3, // PJ
+    4, // PQ
+    5, // PK
+    6, // BJ-T - B=-2,P=-1
+    7, // BQ-T -
+    8, // BK-T
+    // p0
+    9,  // PBJ-T
+    10, // PBQ-T
+    11  // PBK-T
 };
 
-int get_action() {
-  // returns Pass = 3, Bet = 6
-  // doesn't really matter
-  int action;
-  std::cin >> action;
-  return (action + 1) * 3;
-}
+// I need a function that takes a node and an action and finds other nodes
+// needed i.e. 0 requires 4,5+6,7 - no jacks node+3 always pass weird way of
+// calculating around though node+6 is a bet if node%3 == prev_node%3 pass
+//
 
-int showdown(Gamestate &game) {
-  // returns index of winner
-  return game.card[0] < game.card[1];
-}
+// 3 arrays in a struct
 
-// Example
-// Infostate: 5(pK),
-// Strategy:0.5, Terminal:[1,0], EV = [0,X]
-// X=Average EV of all subtrees after bet
+// float regret[12][2], strategy[12];
 
-// opponent moves,
-// Infostate: (player)9 + (action=bet)6 + (0/1) = 15/16
-// 15. Strategy = .5, Terminal = 1, EV = [1,1]
-// 16. Strategy = .5, Terminal = 1, EV = [1,1]
-// X = Strategy*EV
-//   = ((0.5*1 + 0.5*1) + (0.5*1 + 0.5*1) / 2)
-//   = 1
+// Regret will be cumulative
+// strategy[node] = regret[node][0] / regret[node][0] + regret[node][1]
+// Strategy updated at end of loop
+// We need cards in here
+//
 
-// Update infostate
-// Bet always wins with this hand
-// Current EV = 0.5
-// EV always bet = 1
-// EV always pass = 0
-// Regret_bet  = EV of always betting - EV current strategy
-//             = 1 - 0.5= 0.5
-// Regret_pass = 0
-// Cum_Regret = [max(0,0), max(0,0.5)] - Need to only take positive regrets,
-// this added to sum every visit
-//            = [0, 0.5] - Keeps compounding every round, not for these cards
-//            but could be both pos
-// Normalise
-// total regret = 0.5+0 = 0.5 - sum cumulative regrets from both sides
-// Strategy = Strategy / total regret
-// Update strategy all at once I think
-
-// So the worst part is I don't think I need poker
-// I just need a way of traversing the tree :/
-
-// I just need to start at root,
-// Traverse tree
-// Calculate EV at bottom
-// Bubble back up
-
-// Infostate is what the Bot will see
-// Number between 0-18 - 3cards*3actions*2players (1st or second)
-
-// I'm imagining that these will be nodes on our cfr
-// Each infostate will have an associated float array strategy
-// eg. [0.2, 0.8] = [P(pass), P(bet)]
-// We initialise everything at [.5, .5]
-// We make our decision
-// We then calculate regret??
-// Idk how that works
-// Will need to calculate it every round
-
-int play_round(Gamestate &game) {
-  int infostate =
-      (9 * game.player) + 3 * (game.action + 1) + game.card[game.player];
-
-  // Players both ante 1 chip at start
-  // 0 bet/pass - nothing return 1
-  // pass pass - showdown for 2 chips
-  // bet bet - showdown for 4 chips
-  // bet pass- p1 wins 2 chips
-  //
-
-  print("Infostate", infostate);
-  print("Pass(0) or Bet(1)");
-  int action = get_action();
-
-  //
-
-  // Bet bet showdown for 4
-
-  // unless its r0 - passing will end the round with no winner
-  if (!(game.action == 0) and (action == 0)) {
-    // pass pass -
-    print("Passed - ended round");
-    // payoff = 0
-    return 0;
+float regret(int node, int prev_node{
+  // If the node is terminal i.e. > 6, we calculate regret
+  if (node > 5) {
+    // Showdown occurs here if we bet
+    // For <9 - card=prev node - 6, >9 card=prevnode-3
+    //
+    // We need to get the cards and determine a winner
+    //
+    int bet_payout[2] = []
+    //
   }
 
-  // double bets have a showdown
-  if (game.action == 6 and action == 6) {
-    // Win  payoff = 1
-    // Lose payoff = -1
-    int winner = showdown(game);
-    print("Winner", winner);
-    // I'll needa to some regret maxing here I assume
-    return 0;
-  }
-
-  // Any other scenario it gets updated and we repeat
-  game.action = action;
-  game.player = !game.player;
-  return 1;
+  return 0;
 }
 
-int main() {
-  // Deck
-  arr<3> deck = create_deck();
-  Gamestate game;
-
-  print("New Game");
-  int playing;
-  int bb = 0;
-  while (true) {
-    // round initialisation
-    print("New Round");
-    playing = 1;
-    game.player = bb;
-    shuffle_deck(deck);
-    game.card = {deck[game.player], deck[!game.player]};
-    game.action = 0;
-
-    // Play a round
-    while (playing) {
-      playing = play_round(game);
-    }
-
-    bb = !bb;
-  }
+int main(){
   return 0;
 }
