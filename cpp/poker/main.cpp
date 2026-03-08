9ac1b2e4a6aea8220256c1e8c80602c37988ea7f
colmf1 <colmfearon@hotmail.com>
2026-03-08 00:38:10 +0000

    getting somewhere, making separate file for handeval

--- cpp/poker/main.cpp
+++ cpp/poker/main.cpp
@@ -4,6 +4,22 @@
 #include <iostream>
 #include <random>
 
+constexpr std::array<int8_t, 16> make_wheel_lookup() {
+  std::array<int8_t, 16> lookup{};
+  for (int i = 0; i < 16; i++) {
+    int count = 0;
+    while (count < 4 && (i >> count & 1))
+      count++;
+    lookup[i] = count;
+  }
+  return lookup;
+}
+
+constexpr uint64_t MASK13 = 0x1FFF;
+constexpr uint16_t MASK4 = 0xF;
+constexpr uint64_t SWITCH = 1;
+static constexpr std::array<int8_t, 16> WHEEL_LOOKUP = make_wheel_lookup();
+
 struct Gamestate {
   int hand[2][2];
   int table[5];
@@ -24,21 +40,20 @@ void shuffle_deck(std::array<int, 52> &deck) {
 
 void deal_cards(Gamestate &game, std::array<int, 52> deck) {
   shuffle_deck(deck);
-  
+
   game.hand[0][0] = deck[0];
   game.hand[1][0] = deck[1];
   game.hand[0][1] = deck[2];
   game.hand[1][1] = deck[3];
-  
+
   game.table[0] = deck[4];
   game.table[1] = deck[5];
   game.table[2] = deck[6];
   game.table[3] = deck[7];
   game.table[4] = deck[8];
 }
-// Spades are first 13 cards, then clubs, then hearts, then diamonds
-// 0-13: Spades
-uint64_t board(int card_idx) { return 1ULL << card_idx; }
+
+uint64_t board(int card_idx) { return SWITCH << card_idx; }
 
 uint64_t bitboard(int *cards, int n) {
   uint64_t res = 0;
@@ -48,12 +63,12 @@ uint64_t bitboard(int *cards, int n) {
   return res;
 }
 
-const uint64_t suit_masks[4] = {0x1FFFULL, 0x1FFFULL << 13, 0x1FFFULL << 26, 0x1FFFULL << 39};
-
+const uint64_t suit_masks[4] = {MASK13, MASK13 << 13, MASK13 << 26,
+                                MASK13 << 39};
 
 uint64_t rank_mask(int rank) {
-  return (1ULL << rank) | (1ULL << (rank + 13)) | (1ULL << (rank + 26)) |
-         (1ULL << (rank + 39));
+  return (SWITCH << rank) | (SWITCH << (rank + 13)) | (SWITCH << (rank + 26)) |
+         (SWITCH << (rank + 39));
 }
 
 void count_matches(uint64_t hand, std::array<int, 5> &evals) {
@@ -66,25 +81,24 @@ void count_matches(uint64_t hand, std::array<int, 5> &evals) {
   }
 }
 
-void count_straight (uint64_t hand, std::array<int, 5> evals){
-  uint16_t ranks = ((hand) & 0x1FFFULL) | ((hand >> 13) & 0x1FFFULL) |
-                   ((hand >> 26) & 0x1FFFULL) | ((hand >> 39) & 0x1FFFULL);
-  
+void count_straight(uint64_t hand, std::array<int, 5> &evals) {
+  uint16_t ranks = ((hand)&MASK13) | ((hand >> 13) & MASK13) |
+                   ((hand >> 26) & MASK13) | ((hand >> 39) & MASK13);
+
   uint16_t r2 = ranks & (ranks >> 1);
   uint16_t r3 = r2 & (ranks >> 2);
   uint16_t r4 = r3 & (ranks >> 3);
   uint16_t r5 = r4 & (ranks >> 4);
-  
-  bool wheel = (ranks & 0x1001) == 0x1001; // Ace and 2-5 straightA
-  evals[3] = std::max(1 + !!r2 + !!r3 + !!r4 + !!r5, wheel ? 5 : 0);
+
+  int low = WHEEL_LOOKUP[ranks & 0xF];
+  int wheel = (ranks >> 12 & 1) && low ? low + 1 : 0;
+
+  evals[3] = std::max(1 + !!r2 + !!r3 + !!r4 + !!r5, wheel);
 }
 
-void count_flush(uint64_t hand, std::array<int, 5> evals) {
-  // Mask 13 bits
-  // this is just 1s, so we can check for collisions
-  // ULL needed for 64 bit
+void count_flush(uint64_t hand, std::array<int, 5> &evals) {
   int max_suit = 0;
-  for (int suit=0; suit < 3; suit++) {
+  for (int suit = 0; suit < 3; suit++) {
     int count = __builtin_popcountll(hand & suit_masks[suit]);
     if (count > max_suit) {
       max_suit = count;
@@ -95,24 +109,21 @@ void count_flush(uint64_t hand, std::array<int, 5> evals) {
 
 std::array<int, 5> eval_hand(uint64_t hand) {
   std::array<int, 5> evals = {0, 0, 0, 0, 0};
-  // count_matches updates evals inplace, others don't
-  // need to think more abt this - they should all be the same
-  // I think pointers are more efficient
-  // But return logic for count straight is dumb
   count_matches(hand, evals);
   count_straight(hand, evals);
   count_flush(hand, evals);
   return evals;
 }
 
-static const char * ranks[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-static const std::array<std::string, 4> suits = {"Spades", "Clubs", "Hearts", "Diamonds"};
-
+static const char *ranks[] = {"2", "3",  "4", "5", "6", "7", "8",
+                              "9", "10", "J", "Q", "K", "A"};
+static const std::array<std::string, 4> suits = {"Spades", "Clubs", "Hearts",
+                                                 "Diamonds"};
 
 void print_hand(uint64_t hand) {
   while (hand) {
-    int card_idx = __builtin_ctzll(hand);
-    hand &= hand - 1; // removes the bit we just checked 
+    int card_idx = __builtin_ctzll(hand); // grabs first non 0
+    hand &= hand - 1;                     // removes the bit we just checked
     std::cout << ranks[card_idx % 13] << " of " << suits[card_idx / 13] << "\n";
   }
 }
@@ -123,14 +134,14 @@ int main() {
   deal_cards(game, deck);
   uint64_t hand0 = bitboard(game.hand[0], 2);
   uint64_t table = bitboard(game.table, 5);
-  
+
   uint64_t cards = hand0 | table;
-  std::cout << "Hand:\n"; 
+  std::cout << "Hand:\n";
   print_hand(hand0);
   std::cout << "Table:\n";
   print_hand(table);
   std::array<int, 5> res = eval_hand(cards);
-  
+
   std::cout << "No Pairs:" << res[0] << "\n";
   std::cout << "No toak:" << res[1] << "\n";
   std::cout << "No foak:" << res[2] << "\n";
--- cpp/poker/notes.md
+++ cpp/poker/notes.md
@@ -31,6 +31,76 @@ These can be used for both bucketing and hand eval
 my code is a mess and getting compiler warnings but it compiles and runs 
 
 
+
+Wheel straight
+- This is a trick for grabbing the wheel straight 
+- We want to count the number in a row from 2,3,4,5 to check for wheel straight 
+- If we do ranks & 0xF(1111) We get the binary table below 
+- We can then make a lookup table that returns the consecutive bits
+
+```
+constexpr std::array<int8_t, 16> make_wheel_lookup(){
+  // Lookup for our wheel straight
+  // We want to count consecutive ranks from 2-5
+  // If it's not concecutive from 2-> it doesn't matter we break
+  // We loop through all possible combinations of 4 bits, 0000,0001 etc
+  // At each index 
+  // grab the last bit,
+  // if its 1, increment count, check next bit
+  // break at 0
+  std::array<int8_t, 16> lookup{};
+  for (int i = 0; i < 16; i++) {
+      int count = 0;
+      while (count < 4 && (i >> count & 1)) count++;
+      lookup[i] = count;
+  }
+  return lookup;
+}
+
+void count_straight(uint64_t hand, std::array<int, 5> &evals) {
+  // Squash our hand onto 1 line, ignore suits
+  uint16_t ranks = ((hand)&MASK13) | ((hand >> 13) & MASK13) |
+                   ((hand >> 26) & MASK13) | ((hand >> 39) & MASK13);
+  
+  // We & it with shifting it over 1
+  // r2 = 0111 & 0011 = 0011,
+  // r3 = 0011 & 0001 = 0001
+  // later we do !! to turn into 1,0 flag
+  uint16_t r2 = ranks & (ranks >> 1);
+  uint16_t r3 = r2 & (ranks >> 2);
+  uint16_t r4 = r3 & (ranks >> 3);
+  uint16_t r5 = r4 & (ranks >> 4);
+  
+  // This returns no between 1-4,
+  // from 2-5, how many consecutive cards are there 
+  // if there's an ace, and a low streak, we return low+1, otherwise 0
+  int low   = WHEEL_LOOKUP[ranks & 0xF];
+  int wheel = (ranks >> 12 & 1) && low ? low + 1 : 0;
+
+  evals[3] = std::max(1 + !!r2 + !!r3 + !!r4 + !!r5, wheel);
+}
+
+
+```
+
+index → binary → consecutive from bit 0 → LUT value
+0   →  0000  → none                   → 0
+1   →  0001  → 2                      → 1
+2   →  0010  → no 2, chain broken     → 0
+3   →  0011  → 2,3                    → 2
+4   →  0100  → no 2, chain broken     → 0
+5   →  0101  → 2, gap at 3            → 1
+6   →  0110  → no 2, chain broken     → 0
+7   →  0111  → 2,3,4                  → 3
+8   →  1000  → no 2, chain broken     → 0
+9   →  1001  → 2, gap at 3            → 1
+10  →  1010  → no 2, chain broken     → 0
+11  →  1011  → 2,3, gap at 4          → 2
+12  →  1100  → no 2, chain broken     → 0
+13  →  1101  → 2, gap at 3            → 1
+14  →  1110  → no 2, chain broken     → 0
+15  →  1111  → 2,3,4,5                → 4
+
 - Next steps 
     - Add high card to the mix 
     - I want high card within flush straight pairs etc 
